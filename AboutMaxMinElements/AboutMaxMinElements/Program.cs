c037384a2b43973af81c11bbda1c948da0c56b5f
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-04-25 22:19:12 +0400

    About min and max

---
+++ AboutMaxMinElements/AboutMaxMinElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AboutMaxMinElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            WriteLine("Enter the size of mass");
+            int k = int.Parse(ReadLine());
+            int[] arr = new int[k];
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = rd.Next(-100, 100);
+                Write(arr[i]+"\t");
+               
+            }
+            int max = -1;
+            for(int j=0;j<arr.Length;j++)
+            {
+                
+                if(arr[j]>max)
+                {
+                    max = arr[j]; 
+                }
+            }
+            WriteLine("max=" + max+"\n");
+            int min = 1;
+           for(int x=0;x<arr.Length;x++)
+           {
+             if(arr[x]<min)
+             {
+                    min = arr[x];
+             }
+             
+           }
+            WriteLine("min=" + min + "\n");
+        } 
+    }
+}
