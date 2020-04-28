5b058d545b29fd6a793abf1e7f4fbf382b6baf37
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-04-29 02:24:29 +0400

    Signature

---
+++ AddMassElement/AddMassElement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+
+namespace AddMassElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            WriteLine("Enter the size of array");
+            int k = int.Parse(ReadLine());
+            int[] arr = new int[k];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(-100, 100);
+                Write(arr[i]+"\t");
+            }
+            
+            WriteLine("Enter the value for new element");
+            int x = int.Parse(ReadLine());
+            WriteLine("Enter the index of new element");
+            int y = int.Parse(ReadLine());
+            WriteLine("Show the new array");
+            
+            int[] Arr = new int[k + 1];
+            Arr[y] = x;
+            for (int c = 0; c < k-(k-y); c++)
+            {
+                Arr[c] = arr[c];
+                Write(Arr[c]+"\t");
+            }
+            WriteLine("enter the Enter to continue ");
+            ReadLine();
+          
+            for (int z = 0; z <k-(k-y-1); z++)
+            {
+                arr[y] =Arr[y];
+                Arr[y] = x;
+                Arr[z] = arr[z];
+                Write(Arr[z]+"\t");
+                
+            }
+            WriteLine("The end of elements");
+            ReadLine();
+            for (int s = y; s <k+1; s++)
+            {
+                
+                Arr[s] = arr[s];
+                Write(Arr[s]+"\t");
+            }
+            WriteLine("Arr.lenght");
+            for (int j = 0; j <Arr.Length; j++)
+            {
+                Write("Arr[]=" + Arr[j]);
+            }
+
+           
+            
+
+        }
+    }
+}
---
+++ SortedList/SortedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            WriteLine("Enter the size of Mass");
+            int k = int.Parse(ReadLine());
+            int[] arr = new int[k];
+           
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = rd.Next(-100, 100);
+                Write(arr[i]+"\t");
+            
+               
+            }
+            ArrayList numbers = new ArrayList(arr.Length);
+            WriteLine("\n Enter the index of Max");
+            int max = -100;
+            int a = 0;
+            do
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] > max)
+                    {
+                        max = arr[j];
+                    }
+                }
+                WriteLine("\n" + "Max = " + max);
+
+                
+                
+                numbers.RemoveAt(numbers.IndexOf(max));
+                foreach (var n in numbers)
+                {
+                    Write(n);
+                }
+            } while (a<x);
+            
+            ReadLine();
+
+        }
+    }
+}
